54f4eefdeaba1ee5d696e887dc53eae8e9be11af
Anushri <anushri_gupta@outlook.com>
2023-11-09 13:22:06 +0100

    changed password back to tina

--- DataAccess/AppContext.cs
+++ DataAccess/AppContext.cs
@@ -14,7 +14,7 @@ public class AppContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=password;");
+        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=tina;");
     }
 
     public DbSet<Plant> Plants { get; set; }
