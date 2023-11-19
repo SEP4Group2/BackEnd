09886285d9832d11b46122a3c0606f86629ae0ed
Dan Vyhlidal <dan.vyhlidal@chargedmonkey.com>
2023-11-19 18:19:36 +0100

    Deleted the old entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Waiting for the db to get initialized so that migration of data tables is possible in runtime
-while ! pg_isready -h db -p 5432 -q -U admin; do
-  echo "Waiting for PostgreSQL to start..."
-  sleep 2
-done
-
-echo "PostgreSQL finished loading. Starting API..."
-dotnet BackEndAPI.dll
