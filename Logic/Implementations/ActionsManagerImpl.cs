4dd45de81f97942f90b7ff8d2561d36eda4245e0
aleadamczak <ola.adamczak4111@gmail.com>
2023-12-02 19:26:16 +0100

    ﻿ActionsController.cs and ActionsManagerImpl.cs

---
+++ Logic/Implementations/ActionsManagerImpl.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using Logic.Interfaces;
+
+namespace Logic.Implementations;
+
+public class ActionsManagerImpl : IActionsManager
+{
+    public NetworkStream tcpClientStream()
+
+    {
+        string tcpServerIp = "tcpserver";
+        int tcpServerPort = 23;
+
+        TcpClient tcpClient = new TcpClient(tcpServerIp, tcpServerPort);
+
+        return tcpClient.GetStream();
+
+    }
+
+}
